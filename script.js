5da05a9890c89e3ba6a65421b78541b1df22c0cb
Vasil <vasil.todorov@student.fontys.nl>
2022-10-23 19:04:19 +0200

    add enemy movement

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.addEventListener("load", function(){
     canvas.width = 800;
     canvas.height = 720;
 
+    let enemies = [];
+
     class InputHandler {
         constructor(){
             this.keys = [];
@@ -42,8 +44,8 @@ window.addEventListener("load", function(){
             this.weight = 1;
         }
         draw(context){
-            context.fillStyle = "white";
-            context.fillRect(this.x, this.y, this.width, this.height);
+            //context.fillStyle = "white";
+            //context.fillRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
         }
         update(input){
@@ -97,11 +99,37 @@ window.addEventListener("load", function(){
         }
     }
     class Enemy {
-
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 160;
+            this.height = 119;
+            this.image = document.getElementById("enemyImage");
+            this.x = this.gameWidth;
+            this.y = this.gameHeight - this.height;
+            this.frameX = 0;
+            this.speed = 8;
+        }
+        draw(context){
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        }
+        update(){
+            this.x -= this.speed;
+        }
     }
-
-    function handleEnemies(){
-
+        
+    function handleEnemies(deltaTime){
+        if (enemyTimer > enemyInterval + randomEnemyInterval){
+            enemies.push(new Enemy(canvas.width, canvas.height));
+            randomEnemyInterval = Math.random() * 1000 + 500;
+            enemyTimer = 0;
+        } else {
+            enemyTimer += deltaTime;
+        }
+        enemies.forEach(enemy => {
+            enemy.draw(ctx);
+            enemy.update();
+        })
     }
 
     function displayStatusText(){
@@ -112,13 +140,21 @@ window.addEventListener("load", function(){
     const player = new Player(canvas.width, canvas.height);
     const background = new Background(canvas.width, canvas.height);
 
-    function animate(){
+    let lastTime = 0;
+    let enemyTimer = 0;
+    let enemyInterval = 1000;
+    let randomEnemyInterval = Math.random() * 1000 + 500;
+
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0,0, canvas.width, canvas.height);
         background.draw(ctx);
-        background.update();
+        //background.update();
         player.draw(ctx);
         player.update(input);
+        handleEnemies(deltaTime);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 });
