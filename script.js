05fc7ed4fce639ea77fd4a2ceb75adad93da2a63
Vasil <vasil.todorov@student.fontys.nl>
2022-10-23 18:19:49 +0200

    set up moving background

--- script.js
+++ script.js
@@ -76,6 +76,26 @@ window.addEventListener("load", function(){
         }
     }
 
+    class Background {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById("backgroundImage");
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 720;
+            this.speed = 7;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
+        }
+        update(){
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x = 0;
+        }
+    }
     class Enemy {
 
     }
@@ -90,9 +110,12 @@ window.addEventListener("load", function(){
 
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
 
     function animate(){
         ctx.clearRect(0,0, canvas.width, canvas.height);
+        background.draw(ctx);
+        background.update();
         player.draw(ctx);
         player.update(input);
         requestAnimationFrame(animate);
